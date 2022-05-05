6ee6bc4a7703465b55f32b452c6ff56ec98dc8c3
Andreas <63600756+andreasjoensson@users.noreply.github.com>
2022-05-05 15:44:20 +0100

    Add files via upload

---
+++ scrapestartups.py
@@ -0,0 +1,51 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+def scrapeCompanies():
+ f = open('companies.csv', 'a',newline='', encoding='utf-8')
+ writer = csv.writer(f)
+ for x in range(1827, 1829):
+  page = requests.get("https://www.eu-startups.com/directory/page/" + str(x) +"/")
+  soup = BeautifulSoup(page.content, 'html.parser')
+  print("DEN HER SIDE:",x)
+
+  for a in soup.select('#wpbdp-listings-list'):
+     title = a.select('.listing-title > a')
+     for text in title:
+      link = text['href']
+      companyPage = requests.get(link)
+      companySoup = BeautifulSoup(companyPage.content, 'html.parser')
+      hasDescription = companySoup.select_one('.wpbdp-field-business_description > .value')
+      if hasDescription != None:
+       hasLongDescription = companySoup.select_one('.wpbdp-field-long_business_description > .value')
+       if hasLongDescription != None:
+        businessDescription = companySoup.select_one('.wpbdp-field-long_business_description > .value').text
+        cleanText = businessDescription.replace("\n","").strip()
+        industrys = companySoup.select_one('.wpbdp-field-tags > .value')
+        if industrys != None:
+         industrys = companySoup.select_one('.wpbdp-field-tags > .value').text.replace("\n","").strip().split(",")
+         print(cleanText)
+         print(industrys)
+         writer.writerow([cleanText,industrys])
+       else: 
+        businessDescription = companySoup.select_one('.wpbdp-field-business_description > .value').text
+        cleanText = businessDescription.replace("\n","").strip()
+        industrys = companySoup.select_one('.wpbdp-field-tags > .value')
+        if industrys != None:
+         industrys = companySoup.select_one('.wpbdp-field-tags > .value').text.replace("\n","").strip().split(",")
+         print(cleanText)
+         print(industrys)
+         writer.writerow([cleanText,industrys])
+      else:
+       print("IKKE NOGET HER")
+
+ f.close()
+
+
+scrapeCompanies()
+
+
+    
+
+
